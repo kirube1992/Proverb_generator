17bef881fb5146ca5f16b4a1997ec5937746a298
kirubel <kirubelaynalem92@gmail.com>
2025-07-15 14:38:19 +0300

    Updaet some componetnts

---
+++ src/views/ProverbQote.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue';
+import proverbs from '../proverb.json';
+const proverb = ref({});
+function getRandomProverb() {
+ const index = Math.floor(Math.random() * proverbs.length);
+proverb.value = proverb[index];
+}
+
+getRandomProverb();
+
+</script>
+<template>
+    <div class="proverb-quote">
+        <h2>Proverb of the day</h2>
+        <p>{{proverb?.value.text}}</p>
+        <p>{{proverb?.value.reference}}</p>
+        <button @click="getRandomProverb">get new Quote</button>
+    </div>
+</template>
+
+<style scoped>
+    .proverb-quote {
+        color: #ccc;
+        text-align: center;
+        margin: 20px;
+        padding: 20px;
+        border: 1px solid #ccc;
+        border-radius: 8px;
+    }
+</style>
